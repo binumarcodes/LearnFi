8b03d7cd5238ff367295590752e6a500f27d572a
binumarcodes <abdulrahmanbinumar179@gmail.com>
2025-03-28 12:28:07 +0100

    First Commit

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,12 @@
 
-import './Header.css';
+import './header.css'
 import logo from '../../assets/text_logo.png'; // Ensure you have a logo.png file in your project
 
 function Header() {
   return (
-    <header className="header">
+    <div className="header">
       <img src={logo} alt="Logo" className="logo" />
-    </header>
+    </div>
   );
 }
 
