d9e33db2045bdafacb2aab5f1aded3e46fe542f5
binumarcodes <abdulrahmanbinumar179@gmail.com>
2025-04-05 14:18:32 +0100

    Added social links

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -19,7 +19,7 @@ function Footer() {
               <a href="https://x.com/learnfi_app" target="_blank" rel="noopener noreferrer">
                 <FaTwitter />
               </a>
-              <a href="www.linkedin.com/in/learnfi-learnfi-80575b35a" target="_blank" rel="noopener noreferrer">
+              <a href="https://www.linkedin.com/in/learnfi-app-80575b35a/" target="_blank" rel="noopener noreferrer">
                 <FaLinkedin />
               </a>
               <a href="https://discord.com/channels/1358037311321014372/1358039041219624991" target="_blank" rel="noopener noreferrer">
